a38a1bb9e5a596a4af90e5aea63e7170d4f7afe0
Jordan Christensen <jordan.c4922@gmail.com>
2020-02-17 14:41:21 -0700

    Implement bubble sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,7 +1,7 @@
 def selection_sort(arr):
     for i in range(0, len(arr) - 1):
         smallestIndex = i
-        for j in range(i, len(arr)):
+        for j in range(i + 1, len(arr)):
             if arr[smallestIndex] > arr[j]:
                 smallestIndex = j
         
@@ -11,13 +11,22 @@ def selection_sort(arr):
 
     return arr
 
-# print(selection_sort([1, 4, 3, 2, 5, 3, 1, 7, 2, 3, 8, 12, 7]))
+myArr = [1, 4, 3, 2, 5, 3, 1, 7, 2, 3, 8, 12, 7]
+# print("Selection Sort: " + selection_sort(myArr))
 
-# TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
+    for i in range(0, len(arr)):
+        for j in range(0, len(arr) - i - 1):
+            if arr[j] > arr[j+1]:
+                tempVal = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = tempVal
+
+                
 
     return arr
 
+# print("Bubble Sort: " + bubble_sort(myArr))
 
 # STRETCH: implement the Count Sort function below
 def count_sort(arr, maximum =- 1):
